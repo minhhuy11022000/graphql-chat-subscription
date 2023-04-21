1f3f62e22717cb2ab32bcd098d02c4625b29844b
MinHee <dylandang.dev@gmail.com>
2023-04-21 22:41:26 +0700

    configure ApolloClient to use GraphQL over WS for Subscriptions

--- client/src/lib/graphql/client.js
+++ client/src/lib/graphql/client.js
@@ -1,19 +1,49 @@
-import { ApolloClient, ApolloLink, concat, createHttpLink, InMemoryCache } from '@apollo/client';
-import { getAccessToken } from '../auth';
-
-const httpLink = createHttpLink({ uri: 'http://localhost:9000/graphql' });
+import {
+  ApolloClient,
+  ApolloLink,
+  concat,
+  createHttpLink,
+  InMemoryCache,
+  split,
+} from "@apollo/client";
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
+import { createClient as createWsClient } from "graphql-ws";
+import { getMainDefinition } from "@apollo/client/utilities";
+import { getAccessToken } from "../auth";
+import { Kind, OperationTypeNode } from "graphql";
 
 const authLink = new ApolloLink((operation, forward) => {
   const accessToken = getAccessToken();
   if (accessToken) {
     operation.setContext({
-      headers: { 'Authorization': `Bearer ${accessToken}` },
+      headers: { Authorization: `Bearer ${accessToken}` },
     });
   }
   return forward(operation);
 });
 
+// Create new link that can process GraphQL over WebSocket requests
+const wsLink = new GraphQLWsLink(
+  createWsClient({
+    url: "ws://localhost:9000/graphql",
+  })
+);
+
+const httpLink = concat(
+  authLink,
+  createHttpLink({ uri: "http://localhost:9000/graphql" })
+);
+
 export const apolloClient = new ApolloClient({
-  link: concat(authLink, httpLink),
+  link: split(isSubscription, wsLink, httpLink),
   cache: new InMemoryCache(),
 });
+
+function isSubscription(operation) {
+  const definition = getMainDefinition(operation.query);
+  return (
+    // Kind is an enum of GraphQL
+    definition.kind === Kind.OPERATION_DEFINITION &&
+    definition.operation === OperationTypeNode.SUBSCRIPTION
+  );
+}
